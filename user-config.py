0dff5152d0049aa557de11809d4f909d4744ae5d
Tural <48620408+Legendexo@users.noreply.github.com>
2019-05-21 16:23:03 -0500

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -2,3 +2,23 @@
 family = 'wikipedia'
 mylang = 'en'
 usernames['wikipedia']['en'] = u'ExmapleBot'
+
+usernames }
+
+familyy
+else =|
+if 
+sql{ coding"
+    
+entig:trojan\
+    
+    sevingbot:\n\n
+    
+    etoingles"sevingbot"
+    
+    
+    
+    
+    
+    
+   }
